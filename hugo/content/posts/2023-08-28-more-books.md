15d667d90a1bb8213f66a3c94851b872827f0586
Pierre BAILLET <pierre@baillet.name>
2024-08-28 22:20:34 +0200

    Book page + script

---
+++ hugo/content/posts/2023-08-28-more-books.md
@@ -0,0 +1,55 @@
+> A la gorge - Max Monnehay
+> Seuil
+> Date de parution: 09/02/2024
+> 20 €
+> 9782021541823
+> L'homme aux mille visages - Sonia Kronlund
+> Grasset Et Fasquelle
+> Date de parution: 03/01/2024
+> 19 €
+> 9782246831891
+> Stella et l'Amérique - Joseph Incardona
+> Finitude
+> Date de parution: 05/01/2024
+> 21 €
+> 9782363392015
+> Upgrade - Blake Crouch
+> Gallmeister
+> Date de parution: 05/10/2023
+> 24.9 €
+> 9782351783191
+> Wayward Pines Tome 1 : Révélation - Blake Crouch
+> Gallmeister
+> Date de parution: 18/01/2024
+> 7.9 €
+> 9782404080208
+> Brume Tome 2 : la forêt des âmes perdues - Jérôme Pélissier, Carine Hinder
+> Glenat
+> Date de parution: 03/01/2024
+> 12.5 €
+> 9782344056424
+> Le royaume enchanté - Russell Banks
+> Actes Sud
+> Date de parution: 03/01/2024
+> 23.5 €
+> 9782330185374
+> Chiennes de garde - Dahlia de La Cerda
+> Editions Du Sous Sol
+> Date de parution: 01/03/2024 
+> 21.5 €
+> 9782364687189
+> Passage de l'Avenir, 1934 - Alexandre Courban
+> Agullo
+> Date de parution: 25/01/2024 
+> 19.9 €
+> 9782382461051
+> Arca, ou la nouvelle Eden - Van Jensen, Jesse Lonergan
+> 404 Editions
+> Date de parution: 07/03/2024 
+> 22 €
+> 9791032408018
+> Zorro : D'entre les morts - Sean Murphy
+> Urban Comics
+> Date de parution: 21/06/2024 
+> 20 €
+> 9791026826101
---
+++ hugo/isbn.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+read -p "Enter an ISBN: " ISBN
+
+curl -q 'https://www.babelio.com/recherche.php' \
+	-H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:129.0) Gecko/20100101 Firefox/129.0' \
+	-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8' \
+	-H 'Accept-Language: en-US,en;q=0.8,fr;q=0.5,fr-FR;q=0.3' \
+	-H 'Origin: https://www.babelio.com' \
+	-H 'DNT: 1' \
+	-H 'Connection: keep-alive' \
+	-H 'Referer: https://www.babelio.com/recherche.php' \
+	-H 'Cookie: p=FR; disclaimer=1' \
+	-H 'Upgrade-Insecure-Requests: 1' \
+	-H 'Sec-Fetch-Dest: document' \
+	-H 'Sec-Fetch-Mode: navigate' \
+	-H 'Sec-Fetch-Site: same-origin' \
+	-H 'Sec-Fetch-User: ?1' \
+	-H 'Priority: u=0, i' \
+  -F "Recherche=$ISBN" \
+  -F 'recherche=' > page.html
+
+iconv -f ISO-8859-1 -t UTF-8 page.html > pageu.html
+# Extract book titles
+titles=$(rg -o '<a href="/livres/[^"]*" class="titre1" >[^<]+</a>' pageu.html | sed -E 's/.*class="titre1" >([^<]+)<.*/\1/')
+
+# Extract author names
+authors=$(rg -o '<a href="/auteur/[^"]*" class="libelle" >[^<]+</a>' pageu.html | sed -E 's/.*class="libelle" >([^<]+)<.*/\1/')
+
+# Extract book URLs
+urls=$(rg -o '<a href="/livres/[^"]*' pageu.html | sed -E 's/<a href="([^"]*)/\1/')
+
+# Extract book thumbnail URLs
+thumbnails=$(rg -o '<img loading="lazy" src="[^"]*"' pageu.html | sed -E 's/.*src="([^"]*)"/\1/')
+
+echo $titles
+echo $authors
+echo $urls
+echo $thumbnails
+
+rm page.html pageu.html
