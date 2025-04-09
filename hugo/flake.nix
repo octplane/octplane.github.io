b155a468de01889ccee03b3bbef548fb2b154858
Pierre BAILLET <pierre.baillet@sonos.com>
2025-04-09 10:39:04 +0200

    Adding flake for Hugo

---
+++ hugo/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Hugo devshell";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            hugo
+          ];
+        };
+      }
+    );
+}
